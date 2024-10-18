2fcf4c0127306492e9781fa4599c9fc7844bed67
haoguosheng <haoguoshengkf@126.com>
2024-10-18 15:55:14 +0800

    多边形绘制

--- canvas/src/views/regularPolygon.vue
+++ canvas/src/views/regularPolygon.vue
@@ -19,8 +19,8 @@ const radToDeg = r => {
 onMounted(() => {
     if(canvasRef.value) {
         const canvas = canvasRef.value;
-        const canvasWidth = 250
-        const canvasHeight = 250
+        const canvasWidth = 550
+        const canvasHeight = 550
         const ratio = Math.max(window.devicePixelRatio,2);
         canvas.width = canvasWidth * ratio
         canvas.height = canvasHeight * ratio
@@ -29,21 +29,37 @@ onMounted(() => {
 
         const ctx = canvas.getContext('2d');
         if(ctx) {
-            // ctx.fillRect(0, 0, 50, 50)
-            // ctx.save();
-            // ctx.translate(50, 50)
-            // ctx.fillRect(0, 0, 50, 50)
-            // ctx.restore();
-            // // ctx.translate(-50, -50)
-            // ctx.fillRect(25, 25, 50, 50)
+            const drawRegularPolygon = (n:number, x:number, y:number, r:number) => {
+                ctx.beginPath()
+                const a = (Math.PI * 2) / n
+                for (let i = 0; i < n; i++) {
+                    const dx = Math.cos(a * i) * r
+                    const dy = Math.sin(a * i) * r
+                    const tx = x + dx
+                    const ty = y + dy
+                    ctx.lineTo(tx, ty)
+                }
+                ctx.closePath()
+                ctx.stroke()
+            }
+            // drawRegularPolygon(8, 500, 500, 300)
 
-            // ctx.fillRect(100, 100, 50, 50)
-            // ctx.rotate(Math.PI / 4)
-            // ctx.fillRect(100, 100, 50, 50)
-
-            const a = degToRad(45)
-            ctx.transform(Math.cos(a), Math.sin(a), -Math.sin(a), Math.cos(a), 0, 0)
-            ctx.fillRect(100, 100, 50, 50)
+            const drawRegularPolygon2 = (n:number, x:number, y:number, r:number) => {
+                const a = (Math.PI * 2) / n
+                ctx.beginPath()
+                ctx.translate(x, y)
+                for (let i = 0; i < n; i++) {
+                    if (i === 0) {
+                        ctx.moveTo(r, 0)
+                    } else {
+                        ctx.rotate(a)
+                        ctx.lineTo(r, 0)
+                    }
+                }
+                ctx.closePath()
+                ctx.stroke()
+            }
+            drawRegularPolygon2(8, 500, 500, 300)
         }
     }
 });
